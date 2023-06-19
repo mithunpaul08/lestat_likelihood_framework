5027ad6cde6b772efb70d09f8ca4329c2a507f9f
Mitch Paul Mithun <mithunpaul08@gmail.com>
2023-06-19 15:25:19 -0700

    runs end to end Code to find english ltf files only

--- find_english_texts.py
+++ find_english_texts.py
@@ -1,7 +1,20 @@
-DOCUMENT_PROFILE_PATH="/Users/mitch/Downloads/LDC2021E06_KAIROS_Schema_Learning_Corpus_Complex_Event_Source_Data_Part_4_V1.0/docs/document_profile.tab"
+#LDC has english and spanish files. This code will get only the english files since that is the only one to be used in IR
 
+import collections
+
+BASEDIR_PATH="/Users/mitch/Downloads/LDC2021E06_KAIROS_Schema_Learning_Corpus_Complex_Event_Source_Data_Part_4_V1.0/"
+DOCUMENT_PROFILE_PATH="docs/document_profile.tab"
+PARENT_CHILDREN_PATH="docs/parent_children.tab"
+
+
+
+import os
+dirs=os.walk(BASEDIR_PATH)
+
+
+#get all the names of parent files which are in english
 only_english_filenames=[]
-with open(DOCUMENT_PROFILE_PATH) as f:
+with open(os.path.join(BASEDIR_PATH,DOCUMENT_PROFILE_PATH)) as f:
     lines=f.readlines()
     for line in lines:
         line_split = line.split("\t")
@@ -10,5 +23,40 @@ with open(DOCUMENT_PROFILE_PATH) as f:
             filename=line_split[2]
             if "english" in language.lower():
                 only_english_filenames.append(filename.strip())
+counter=collections.Counter(only_english_filenames)
 
-print(len(only_english_filenames))
+#get all the names of parent files which are in english
+only_english_filenames=[]
+with open(os.path.join(BASEDIR_PATH,DOCUMENT_PROFILE_PATH)) as f:
+    lines=f.readlines()
+    for line in lines:
+        line_split = line.split("\t")
+        if line_split[1] and line_split[2]:
+            language=line_split[1]
+            filename=line_split[2]
+            if "english" in language.lower():
+                only_english_filenames.append(filename.strip())
+counter=collections.Counter(only_english_filenames)
+
+
+#get all the parent child maps
+parent_child={}
+with open(os.path.join(BASEDIR_PATH,PARENT_CHILDREN_PATH)) as f:
+    lines = f.readlines()
+    for line in lines:
+        line_split = line.split("\t")
+        parent=line_split[2]
+        child=line_split[3]
+        if parent in parent_child:
+            parent_child[parent].append(child)
+        else:
+            parent_child[parent]=[child]
+
+all_english_language_ltf_files=[]
+for each_english_parent in counter.keys():
+    if each_english_parent in parent_child:
+        all_english_language_ltf_files.extend(parent_child[each_english_parent])
+
+
+
+print(len(all_english_language_ltf_files))
