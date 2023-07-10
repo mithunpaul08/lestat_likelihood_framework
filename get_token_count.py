77fa6f0f36335bb5ee9d0a136d1d1de1f1bb2d0e
Mitch Paul Mithun <mithunpaul08@gmail.com>
2023-07-10 14:08:30 -0700

    With actual number of events from schema

--- get_token_count.py
+++ get_token_count.py
@@ -1,4 +1,5 @@
-
+events_per_schema = [22,9,5,6,31,10,12,9,10,9,21,11,13,18,12,20,11,21,15,38,6,12,15,12]
+average_tokens_per_event = 15
 
 with open("data/ir_data/retrieved_docs_20perquery.txt") as f:
     docs = []
@@ -14,22 +15,18 @@ with open("data/ir_data/retrieved_docs_20perquery.txt") as f:
                     paragraphs.append(line)
     docs= " ".join(full_docs).split("#####################")
     doc_token_counter=0
-    events = [0]*10
     for doc in docs:
-        for event in events:
-            prompt = "does the passage" + doc+ "entail, contradict or is neutral to the sentence" \
-                     +"A phisher sends a message to a target's email address" \
-                     + " Answer should be one of entail, contradict or neutral. Don't retype the whole passage again."
-            doc_token_counter += len(prompt.split(" "))
-
+        for event_count in events_per_schema:
+                prompt = "does the passage" + doc+ "entail, contradict or is neutral to the sentence" \
+                         + " Answer should be one of entail, contradict or neutral. Don't retype the whole passage again."
+                doc_token_counter += event_count* len(prompt.split(" "))+average_tokens_per_event
     print(f"number of tokens in all documents= {doc_token_counter}")
 
     para_token_counter = 0
     for para in paragraphs:
-        for event in events:
+        for event in events_per_schema:
             prompt = "does the passage" + para + "entail, contradict or is neutral to the sentence" \
-                     +"A phisher sends a message to a target's email address" \
                      + " Answer should be one of entail, contradict or neutral. Don't retype the whole passage again."
-            para_token_counter += len(prompt.split(" "))
+            para_token_counter += event_count* len(prompt.split(" "))+average_tokens_per_event
 
-    print(f"number of tokens in all documents= {para_token_counter}")
+    print(f"number of tokens in all paragraphs= {para_token_counter}")
